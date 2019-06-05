2913d65043be5eef033e31902951b50088694a4f
lc <591081512@qq.com>
2019-06-05 18:26:45 +0800

    利用MapReducer将HDFS上的数据写入到HBase中

--- src/main/java/com/atguigu/HdfsDataHbas/Txt2FruitRunner.java
+++ src/main/java/com/atguigu/HdfsDataHbas/Txt2FruitRunner.java
@@ -1,12 +1,14 @@
 package com.atguigu.HdfsDataHbas;
 
 
+import com.atguigu.MapReduc.DataTransferDriver;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.util.Tool;
@@ -21,53 +23,30 @@ import java.io.IOException;
  * @create: 2019-06-04 20:20
  * @Version 1.0
  */
-public class Txt2FruitRunner extends Configuration implements Tool {
-    public int run(String[] args) throws Exception {
-        //得到Configuration
-        Configuration conf = this.getConf();
+public class Txt2FruitRunner {
+    public static  void  main(String[] args) throws Exception {
+        Configuration conf = HBaseConfiguration.create();
 
-        //创建Job任务
-        Job job = Job.getInstance(conf, this.getClass().getSimpleName());
+        Job job = Job.getInstance(conf);
+
+        job.setJobName("hbasemr12");
+
         job.setJarByClass(Txt2FruitRunner.class);
-        Path inPath = new Path("hdfs://hadoop101:9000/input_fruit/fruit.tsv");
-        FileInputFormat.addInputPath(job, inPath);
 
-        //设置Mapper
         job.setMapperClass(ReadFruitFromHDFSMapper.class);
+
         job.setMapOutputKeyClass(ImmutableBytesWritable.class);
+
         job.setMapOutputValueClass(Put.class);
 
-        //设置Reducer
+        //hdfs://hadoop101:9000/movie hdfs地址
+        FileInputFormat.setInputPaths(job, new Path("hdfs://hadoop101:9000/fruit.tsv"));
+
+        //movie 为Hbase数据库
         TableMapReduceUtil.initTableReducerJob("fruit2", WriteFruitMRFromTxtReducer.class, job);
 
-         //设置Reduce数量，最少1个
-        job.setNumReduceTasks(1);
+        job.waitForCompletion(true);
 
-        boolean isSuccess = job.waitForCompletion(true);
-        if(!isSuccess){
-           throw new IOException("Job running with error");
-        }
-
-        return isSuccess ? 0 : 1;
     }
-
-
-    public static void main(String[] args) throws Exception {
-        Configuration conf = HBaseConfiguration.create();
-        int status = ToolRunner.run(conf, new Txt2FruitRunner(), args);
-        System.exit(status);
-    }
-
-    Configuration configuration =null;
-    @Override
-    public void setConf(Configuration conf) {
-        configuration=conf;
-    }
-
-    @Override
-    public Configuration getConf() {
-        return configuration;
-    }
-
 
 }
